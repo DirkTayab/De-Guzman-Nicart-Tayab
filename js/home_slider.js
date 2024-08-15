7b2d4ad38a089cd413dbd54a020da1d588acb415
Daniel-1125 <javierdaniel1125@gmail.com>
2024-08-15 15:00:41 +0800

    ben commit

--- js/home_slider.js
+++ js/home_slider.js
@@ -1,20 +1,22 @@
 var slider = tns({
   container: ".homeServices2__slider",
   items: 3,
-    speed: 500,
-    gutter: 100,
-    slideBy: "1",
-    autoplay: true,
-    navPosition: "bottom",
-    containerControls: "#controls",
-    prevButton: ".prev",
-    nextButton: ".next",
-    responsive: {
-        1250: {
-            items: 3,
-        },
-        200: {
-            items: 1,
-        },
+  speed: 500,
+  gutter: 30,
+  slideBy: "1",
+  rewind: true,
+
+  containerControls: "#controls",
+  prevButton: ".homeServices2__prev",
+  nextButton: ".homeServices2__next",
+  responsive: {
+    1250: {
+      items: 3,
     },
+    200: {
+      items: 1,
+    },
+  },
 });
+
+
