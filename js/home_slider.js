a22e1da6db25dec731b63ca42e2ac9dd29f01392
liempot <leemagno9@gmail.com>
2024-08-15 12:26:14 +0800

    df

--- js/home_slider.js
+++ js/home_slider.js
@@ -1,20 +1,44 @@
 var slider = tns({
   container: ".homeServices2__slider",
   items: 3,
-    speed: 500,
-    gutter: 100,
-    slideBy: "1",
-    autoplay: true,
-    navPosition: "bottom",
-    containerControls: "#controls",
-    prevButton: ".prev",
-    nextButton: ".next",
-    responsive: {
-        1250: {
-            items: 3,
-        },
-        200: {
-            items: 1,
-        },
+  speed: 500,
+  gutter: 30,
+  slideBy: "1",
+  rewind: true,
+
+  //   navPosition: "top",
+  containerControls: "#controls",
+  prevButton: ".homeServices2__prev",
+  nextButton: ".homeServices2__next",
+  responsive: {
+    1250: {
+      items: 3,
     },
+    200: {
+      items: 1,
+    },
+  },
+});
+
+var slider = tns({
+  container: ".homeBanner__slider",
+  items: 1,
+  autoheight: "true",
+  speed: 500,
+  slideBy: "1",
+  rewind: true,
+  gutter: 0,
+  axis: "vertical",
+  //   navPosition: "top",
+  containerControls: "#controls",
+  prevButton: ".homeServices2__prev",
+  nextButton: ".homeServices2__next",
+  responsive: {
+    1250: {
+      items: 1,
+    },
+    200: {
+      items: 1,
+    },
+  },
 });
